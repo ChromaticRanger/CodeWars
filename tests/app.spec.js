2bddd9fd7e726de1d2cfa5da45f154578132d6df
marti <marti@SIDEARM>
2022-05-31 22:44:55 +0100

    Set up mocha, chai and nyc. Added add function together with a test spec

---
+++ tests/app.spec.js
@@ -0,0 +1,16 @@
+
+const assert = require('assert')
+const { expect } = require('chai')
+const { add } = require('../app')
+
+describe('the add function', function() {
+
+    it('should add 2 numbers together', function() {
+        expect(add(4, 4)).to.be.eq(8)
+    })
+
+    it('should be able to handle one number', function() {
+        expect(add(2)).to.be.eq(2)
+    })
+
+});
